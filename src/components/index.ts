cb57c2ca271f6821792ce5329ff69f0022f2b829
Sito8943 <sito8943@gmail.com>
2024-08-20 08:08:59 -0400

    exporting chevron

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,12 @@
 import { Table, TableOptionsProvider, useTableOptions } from "./Table";
+import { ChevronDown, ChevronUp } from "./Chevron";
 import { Loading } from "./Loading";
 
-export { Loading, TableOptionsProvider, useTableOptions, Table };
+export {
+  Loading,
+  TableOptionsProvider,
+  useTableOptions,
+  Table,
+  ChevronDown,
+  ChevronUp,
+};
