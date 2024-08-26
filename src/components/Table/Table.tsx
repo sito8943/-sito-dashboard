21cae3bf4ce9a5d3a01d62aeade0f0b8e54e8218
Sito8943 <sito8943@gmail.com>
2024-08-26 10:04:52 -0400

    fixed loading conditional and 0.0.4

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -45,58 +45,69 @@ export function Table(props: TablePropsType) {
           {rows?.length && !isLoading && <PageSize />}
         </div>
       </div>
-      <div className={`${contentClassName} overflow-auto`}>
-        {!rows?.length && !isLoading ? (
-          <Empty />
-        ) : (
-          <table className="w-full text-sm text-left text-gray-500">
-            <Columns
-              entity={entity}
-              columns={columns}
-              columnsOptions={columnsOptions}
-              hasAction={actions?.length > 0}
-            />
-            {!isLoading && Boolean(rows?.length) && (
-              <tbody>
-                {parsedRows?.map((row) => (
-                  <tr
-                    key={row.id}
-                    className={`border-b ${row.deleted.value ? "bg-secondary/10" : "bg-white"}`}
-                  >
-                    {columns?.map((column, i) => (
-                      <td
-                        key={column.key}
-                        className={`px-6 py-4 font-medium ${i === 0 ? "text-gray-900 whitespace-nowrap" : ""} ${columnsOptions?.columnClassNames ? columnsOptions?.columnClassNames[column.key] : ""}`}
-                      >
-                        {row[column.key]?.render ?? row[column.key]}
-                      </td>
-                    ))}
-                    {Boolean(actions.length) && (
-                      <td>
-                        <div className="flex items-center gap-3 w-full justify-center">
-                          {actions
-                            .filter(
-                              (action) => !action.hidden || !action.hidden(row)
-                            )
-                            ?.map((action) => (
-                              <Tooltip key={action.id} content={action.tooltip}>
-                                <button onClick={() => action.onClick(row)}>
-                                  {action.icon}
-                                </button>
-                              </Tooltip>
-                            ))}
-                        </div>
-                      </td>
-                    )}
-                  </tr>
-                ))}
-              </tbody>
-            )}
-          </table>
-        )}
-      </div>
-      {isLoading && <Loading className="bg-white top-0 left-0 w-full h-full" />}
-      {!isLoading && rows?.length && <Navigation />}
+      {!isLoading ? (
+        <div className={`${contentClassName} overflow-auto`}>
+          {!rows?.length ? (
+            <Empty />
+          ) : (
+            <table className="w-full text-sm text-left text-gray-500">
+              <Columns
+                entity={entity}
+                columns={columns}
+                columnsOptions={columnsOptions}
+                hasAction={actions?.length > 0}
+              />
+              {!isLoading && Boolean(rows?.length) && (
+                <tbody>
+                  {parsedRows?.map((row) => (
+                    <tr
+                      key={row.id}
+                      className={`border-b ${row.deleted.value ? "bg-secondary/10" : "bg-white"}`}
+                    >
+                      {columns?.map((column, i) => (
+                        <td
+                          key={column.key}
+                          className={`px-6 py-4 font-medium ${i === 0 ? "text-gray-900 whitespace-nowrap" : ""} ${columnsOptions?.columnClassNames ? columnsOptions?.columnClassNames[column.key] : ""}`}
+                        >
+                          {row[column.key]?.render ?? row[column.key]}
+                        </td>
+                      ))}
+                      {Boolean(actions.length) && (
+                        <td>
+                          <div className="flex items-center gap-3 w-full justify-center">
+                            {actions
+                              .filter(
+                                (action) =>
+                                  !action.hidden || !action.hidden(row)
+                              )
+                              ?.map((action) => (
+                                <Tooltip
+                                  key={action.id}
+                                  content={action.tooltip}
+                                >
+                                  <button onClick={() => action.onClick(row)}>
+                                    {action.icon}
+                                  </button>
+                                </Tooltip>
+                              ))}
+                          </div>
+                        </td>
+                      )}
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
+          )}
+        </div>
+      ) : (
+        <>
+          {isLoading && (
+            <Loading className="bg-white top-0 left-0 w-full h-full" />
+          )}
+          {!isLoading && rows?.length && <Navigation />}
+        </>
+      )}
     </div>
   );
 }
