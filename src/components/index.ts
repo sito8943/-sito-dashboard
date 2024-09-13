6b76ddd091ab5f229d44e881d65a494e46911357
Sito8943 <sito8943@gmail.com>
2024-09-13 09:17:27 -0400

    fixed export of table action ver 0.0.8

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 import { Table, TableOptionsProvider, useTableOptions } from "./Table";
 import { ChevronDown, ChevronUp } from "./Chevron";
 import { Loading } from "./Loading";
-
+export type { TableAction } from "./Table";
 export {
   Loading,
   TableOptionsProvider,
