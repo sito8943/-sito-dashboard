0296bee519b976ef497b1c070eba91352deec8d6
Sito <sito8943@gmail.com>
2025-04-21 18:22:43 -0400

    tooltip full taidinwss

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -10,9 +10,9 @@ export function Tooltip(props: TooltipPropsType) {
   const { content, children } = props;
 
   return (
-    <div className="tooltip-container">
+    <div className="group relative inline-block cursor-pointer">
       {children}
-      <div className="tooltip-text">{content}</div>
+      <div className="invisible opacity-0 group-hover:visible group-hover:opacity-1 bg-black text-gray-50 text-center px-1 py-2 rounded-1 absolute z-1 bottom-[125%] left-[50%] -translate-x-[50%]">{content}</div>
     </div>
   );
 }
