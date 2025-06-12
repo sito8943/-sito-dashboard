72818ab93d88c5d3a82fd9581539efbc0c76ff5b
Sito <sito8943@gmail.com>
2025-06-12 08:48:42 -0400

    added 144

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        144: "576px",
+      },
+    },
   },
   plugins: [],
 };
