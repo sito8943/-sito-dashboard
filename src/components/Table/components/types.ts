71e8af8627bee989760909c26bd95cc080b7203a
Sito <sito8943@gmail.com>
2025-06-15 19:40:40 -0400

    moved table component types to components fiolders

---
+++ src/components/Table/components/types.ts
@@ -0,0 +1,39 @@
+import { FilterTypes, SortOrder } from "lib";
+import { Action } from "../types";
+
+export type ColumnType = {
+  key: string;
+  label: string;
+  sortable?: boolean;
+  sortOptions: {
+    icons: {
+      className: string;
+      asc: string;
+      desc: string;
+    };
+  };
+  className?: string;
+  renderBody?: (value: any, row: any) => React.ReactNode;
+  renderHead?: () => void;
+  filterOptions?: ColumnFilterOptions;
+};
+
+export type ColumnPropsType = {
+  entity: string;
+  columns: ColumnType[];
+  hasAction: boolean;
+  onSortCallback: (prop: string, sortOrder: SortOrder) => void;
+};
+
+export type ColumnFilterOptions = {
+  type: FilterTypes;
+  defaultValue: any;
+  label?: string;
+};
+
+export type RowsPropsType = {
+  data: any[];
+  columns: ColumnType[];
+  softDeleteProperty: string;
+  actions: ((row: any) => Action[]) | undefined;
+};
--- src/components/Table/types.ts
+++ src/components/Table/types.ts
@@ -1,4 +1,5 @@
-import { FilterTypes, SortOrder } from "lib";
+import { SortOrder } from "lib";
+import { ColumnType } from "./components";
 
 export type Action = {
   id: string;
@@ -8,50 +9,14 @@ export type Action = {
   hidden: (entity: object) => boolean;
 };
 
-export type ColumnsOptionsType = {
-  noSortableColumns: {
-    [key: string]: boolean;
-  };
-  columnClassNames: {
-    [key: string]: string;
-  };
-  icons: {
-    className: string;
-    asc: string;
-    desc: string;
-  };
-};
-
-export type ColumnType = {
-  key: string;
-  label: string;
-  filterOptions?: ColumnFilterOptions;
-};
-
-export type ColumnFilterOptions = {
-  type: FilterTypes;
-  defaultValue: any;
-  label?: string;
-};
-
-export type ColumnPropTypes = {
-  entity: string;
-  columns: ColumnType[];
-  columnsOptions?: ColumnsOptionsType;
-  hasAction: boolean;
-  onSortCallback: (prop: string, sortOrder: SortOrder) => void;
-};
-
 export type TablePropsType = {
   entity: string;
   title?: string;
   subtitle?: string;
-  rows: object[];
-  parseRows?: any;
+  data: object[];
   isLoading?: boolean;
   actions?: (row: object) => Action[];
   columns?: ColumnType[];
-  columnsOptions?: ColumnsOptionsType;
   contentClassName?: string;
   className?: string;
   softDeleteProperty?: string;
