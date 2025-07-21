784f98899e8d158184a420b2cfb16f2d1bf0abac
Sito <sito8943@gmail.com>
2025-07-20 20:50:03 -0400

    fixed dark mode selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   corePlugins: {
     preflight: false,
