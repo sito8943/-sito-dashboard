e5dbceeb1ea7a9d7904ebdb442b224bd72b355ae
Sito <sito8943@gmail.com>
2025-07-30 08:47:01 -0400

    no variants on chip

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -4,7 +4,7 @@ import { useMemo } from "react";
 import { Close } from "components";
 
 // types
-import { ChipPropsType, ChipVariant } from "./types";
+import { ChipPropsType } from "./types";
 
 // styles
 import "./styles.css";
@@ -15,43 +15,14 @@ import "./styles.css";
  * @returns Chip component
  */
 export function Chip(props: ChipPropsType) {
-  const {
-    label,
-    onDelete,
-    className = "",
-    spanClassName = "",
-    variant = ChipVariant.default,
-  } = props;
-
-  const localVariant = useMemo(() => {
-    switch (variant) {
-      case ChipVariant.empty:
-        return "text-primary bg-transparent";
-      case ChipVariant.outlined:
-        return "border border-primary";
-      case ChipVariant.default:
-      default:
-        return "text-white bg-primary";
-    }
-  }, [variant]);
-
-  const svgStyle = useMemo(() => {
-    switch (variant) {
-      case ChipVariant.empty:
-      case ChipVariant.outlined:
-        return "chip-delete-button-svg";
-      case ChipVariant.default:
-      default:
-        return "filled-chip-delete-button-svg";
-    }
-  }, [variant]);
+  const { label, onDelete, className = "", spanClassName = "" } = props;
 
   return (
-    <div className={`chip-main ${localVariant} ${className}`}>
+    <div className={`chip-main ${className}`}>
       <span className={spanClassName}>{label}</span>
       {onDelete ? (
         <button type="button" className="chip-delete-button" onClick={onDelete}>
-          <Close className={svgStyle} />
+          <Close />
         </button>
       ) : null}
     </div>
--- src/components/Chip/types.ts
+++ src/components/Chip/types.ts
@@ -1,13 +1,6 @@
 import { MouseEventHandler, ReactNode } from "react";
 
-export enum ChipVariant {
-  empty = "empty",
-  outlined = "outlined",
-  default = "default",
-}
-
 export type ChipPropsType = {
-  variant?: ChipVariant;
   label?: string | ReactNode;
   onDelete?: MouseEventHandler<HTMLElement>;
   className?: string;
