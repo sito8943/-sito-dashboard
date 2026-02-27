4f24509382e513cfd94d97fc614be066e6b318aa
Sito <40501794+sito8943@users.noreply.github.com>
2026-02-27 23:22:46 +0100

    refactor on badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,8 +1,6 @@
 // styles
 import "./styles.css";
 
-import { useEffect, useRef } from "react";
-
 // types
 import { BadgePropsType } from "./types";
 
@@ -14,16 +12,5 @@ import { BadgePropsType } from "./types";
 export const Badge = (props: BadgePropsType) => {
   const { count, className = "" } = props;
 
-  const spanRef = useRef<HTMLSpanElement | null>(null);
-
-  useEffect(() => {
-    if (spanRef.current?.parentNode)
-      (spanRef.current?.parentNode as HTMLElement).style.position = "relative";
-  }, [spanRef]);
-
-  return (
-    <span ref={spanRef} className={`${className} badge-main`}>
-      {count}
-    </span>
-  );
+  return <span className={`${className} badge-main`}>{count}</span>;
 };
